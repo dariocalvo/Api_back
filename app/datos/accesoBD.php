544329238d12b936d06d7cfcf8efa8b55c82818c
Dario Calvo <humildin2011@gmail.com>
2021-07-08 01:37:40 -0300

    Cambio datos expuestos a variables de entorno

--- app/datos/accesoBD.php
+++ app/datos/accesoBD.php
@@ -3,30 +3,14 @@
 class ConeccionBD
 {
 
-    private $servidor;
-    private $nombre_BD;
-    private $user;
-    private $password;
     private static $objAccesoDatos;
     private $objetoPDO;
 
     private function __construct()
     {
-    /*
-        $this->servidor = "localhost";
-        $this->nombre_BD = "sistema_calvo";
-        $this->user = "root";
-        $this->password = "";
-    */  
-    
-        $this->servidor = "remotemysql.com";
-        $this->nombre_BD = "qdg8wnFsb4";
-        $this->user = "qdg8wnFsb4";
-        $this->password = "NZN0iuhEJJ";
     
         try {
-            $this->objetoPDO = new PDO('mysql:host='.$this->servidor.';dbname='.$this->nombre_BD.';charset=utf8',$this->user,$this->password, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        //    $this->objetoPDO->exec("SET CHARACTER SET utf8");
+            $this->objetoPDO = new PDO('mysql:host='.getenv('ServidorMySQL').';dbname='.getenv('Database').';charset=utf8', getenv("Usuario"), getenv('Pass'), array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         } catch (PDOException $e) {
             print "Error: " . $e->getMessage();
             die();
